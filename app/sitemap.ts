77528414edc3783e9bb108520029f9fa5e30c404
oh108899 <oh108899@gmail.com>
2026-03-04 15:51:09 +0900

    fix: 사이트맵 모든 url 로 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,38 @@
-export default function sitemap() {
-return [
-{
-url: "https://nyamnyam-box.vercel.app/",
-lastModified: new Date(),
-},
-];
+import { createClient } from "@/app/utils/supabase/client";
+
+export default async function sitemap(){
+  const Url = "https://nyamnyam-box.vercel.app";
+  const supabase = createClient();
+
+  const { data: recipes } = await supabase
+    .from("recipes")
+    .select("id, updated_at, created_at");
+
+  const recipeUrls =
+    recipes?.map((recipe) => ({
+      url: `${Url}/recipes/${recipe.id}`,
+      lastModified: recipe.updated_at
+        ? new Date(recipe.updated_at)
+        : new Date(recipe.created_at)
+    })) ?? [];
+
+  return [
+    {
+      url: `${Url}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${Url}/recipes`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${Url}/search`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${Url}/search/result`,
+      lastModified: new Date(),
+    },
+    ...recipeUrls,
+  ];
 }
