119c2fb769df5abf7a525f23e6b6163291ad58e4
oh108899 <oh108899@gmail.com>
2026-03-04 13:37:44 +0900

    feat: 구글 서치 콘솔 메타데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "냠냠박스",
   description: "AI를 통한 레시피 공유 사이트",
+  verification: {
+    google: 'Xx4JNfYBY4kWvTI1u5DnN8uB6FN4ddd2vG6IU39vX5w', 
+  },
 };
 
 export default function RootLayout({
